e170b0c3f94e8ad4eae34287f7be43605146a477
Radovan Bast <rbast@users.noreply.github.com>
2014-06-26 22:47:04 +0200

    adapt shell to one-electron integrals

--- shell.py
+++ shell.py
@@ -11,11 +11,20 @@ def get_ijk_list(m):
 
 #-------------------------------------------------------------------------------
 
-def get_shell(a, b, c, d):
+def get_shell4(a, b, c, d):
     components = []
     for p in get_ijk_list(a):
         for q in get_ijk_list(b):
             for r in get_ijk_list(c):
                 for s in get_ijk_list(d):
                     components.append(p + q + r + s)
+    return components
+
+#-------------------------------------------------------------------------------
+
+def get_shell2(a, b):
+    components = []
+    for p in get_ijk_list(a):
+        for q in get_ijk_list(b):
+            components.append(p + q)
     return components
