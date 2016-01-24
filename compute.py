52ff0fa4d470eeb7a41d932078b2e4d9a54d05e5
bast <bast@users.noreply.github.com>
2016-01-24 21:14:37 +0100

    introduce main function

--- compute.py
+++ compute.py
@@ -4,18 +4,24 @@ from itertools import product
 from shell import get_shell4
 from obara_saika import get_coulomb
 
-za = 1.1
-zb = 1.2
-zc = 1.3
-zd = 1.4
 
-ra = [1.0, 0.0, 1.0]
-rb = [0.0, 1.0, 2.0]
-rc = [0.0, 0.0, 3.0]
-rd = [0.0, 0.0, 4.0]
+def main():
+    za = 1.1
+    zb = 1.2
+    zc = 1.3
+    zd = 1.4
 
-# get all integrals up to pppp
-for p in product('01', repeat=4):
-    for c in get_shell4(int(p[0]), int(p[1]), int(p[2]), int(p[3])):
-        if sum(c) > 0:
-            print (c, get_coulomb(za, zb, zc, zd, ra, rb, rc, rd, c))
+    ra = [1.0, 0.0, 1.0]
+    rb = [0.0, 1.0, 2.0]
+    rc = [0.0, 0.0, 3.0]
+    rd = [0.0, 0.0, 4.0]
+
+    # get all integrals up to pppp
+    for p in product('01', repeat=4):
+        for c in get_shell4(int(p[0]), int(p[1]), int(p[2]), int(p[3])):
+            if sum(c) > 0:
+                print (c, get_coulomb(za, zb, zc, zd, ra, rb, rc, rd, c))
+
+
+if __name__ == '__main__':
+    main()
